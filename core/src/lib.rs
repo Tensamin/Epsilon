e499e15628fe7e3596d30dac21687f96d7679b14
Alex Emmet <111742636+Alex-Emmet@users.noreply.github.com>
2026-02-27 16:41:42 +0100

    Update lib.rs

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,14 +3,3 @@ pub mod communication;
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
